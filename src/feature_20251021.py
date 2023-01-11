45a1fdea3892da8489b99e719e287fe794de3359
0xTheDad <ekitaeff@yandex.ru>
2023-01-11 12:43:00 +0300

    Update documentation

--- src/feature_20251021.py
+++ src/feature_20251021.py
@@ -299,3 +299,8 @@ def historical_feature():
     """Feature added on 2023-08-13 13:45:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-01-11 12:43:00
+def historical_feature():
+    """Feature added on 2023-01-11 12:43:00"""
+    print('Historical feature working')
+    return True
