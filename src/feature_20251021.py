02714c66ab6ea815535ec491845c40565691cd37
0xTheDad <ekitaeff@yandex.ru>
2023-05-02 12:05:00 +0300

    Fix security issue

--- src/feature_20251021.py
+++ src/feature_20251021.py
@@ -199,3 +199,8 @@ def historical_feature():
     """Feature added on 2024-06-06 19:21:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-05-02 12:05:00
+def historical_feature():
+    """Feature added on 2023-05-02 12:05:00"""
+    print('Historical feature working')
+    return True
