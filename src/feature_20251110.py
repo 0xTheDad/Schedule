e86ff052b1f108cf59f249f87c2d0175e234fb31
0xTheDad <ekitaeff@yandex.ru>
2023-09-10 10:57:00 +0300

    Add tests

--- src/feature_20251110.py
+++ src/feature_20251110.py
@@ -169,3 +169,8 @@ def historical_feature():
     """Feature added on 2023-04-04 21:30:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-09-10 10:57:00
+def historical_feature():
+    """Feature added on 2023-09-10 10:57:00"""
+    print('Historical feature working')
+    return True
