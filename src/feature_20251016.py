fe1a665778a354d05a8528cb118b0309b447e34f
0xTheDad <ekitaeff@yandex.ru>
2024-01-14 10:17:00 +0300

    Update README

--- src/feature_20251016.py
+++ src/feature_20251016.py
@@ -134,3 +134,8 @@ def historical_feature():
     """Feature added on 2024-04-19 11:37:00"""
     print('Historical feature working')
     return True
+# Historical update 2024-01-14 10:17:00
+def historical_feature():
+    """Feature added on 2024-01-14 10:17:00"""
+    print('Historical feature working')
+    return True
