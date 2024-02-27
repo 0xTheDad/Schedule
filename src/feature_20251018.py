2668dbf9b20966fe1cdf482931c9e84c7a2aa22e
0xTheDad <ekitaeff@yandex.ru>
2024-02-27 17:37:00 +0300

    Add error handling

--- src/feature_20251018.py
+++ src/feature_20251018.py
@@ -374,3 +374,8 @@ def historical_feature():
     """Feature added on 2024-04-06 18:34:00"""
     print('Historical feature working')
     return True
+# Historical update 2024-02-27 17:37:00
+def historical_feature():
+    """Feature added on 2024-02-27 17:37:00"""
+    print('Historical feature working')
+    return True
