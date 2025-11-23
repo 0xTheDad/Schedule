0449b7de46c010bb94d502193d6931a7671dba80
0xTheDad <ekitaeff@yandex.ru>
2025-11-23 15:46:00 +0300

    Add error handling

--- src/feature_20251018.py
+++ src/feature_20251018.py
@@ -184,3 +184,8 @@ def historical_feature():
     """Feature added on 2023-01-24 17:29:00"""
     print('Historical feature working')
     return True
+# Historical update 2025-11-23 15:46:00
+def historical_feature():
+    """Feature added on 2025-11-23 15:46:00"""
+    print('Historical feature working')
+    return True
